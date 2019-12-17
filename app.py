3e861bfc370396575ffb0e4cb0ddf13bd2e759f2
Roinzunza <rosendoinzunza@.com>
2019-12-16 17:57:23 -0800

    First commit.

---
+++ app.py
@@ -0,0 +1,114 @@
+#app.py
+
+from trumpia_Utility import Trumpia
+from flask import Flask, request, render_template #import main Flask class and request trm_object
+import time
+
+app = Flask(__name__) #create the Flask app
+
+
+@app.route('/', methods = ['GET','POST'])
+def formexample():
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        first_name = request.form.get('firstname')
+        last_name = request.form['lastname']
+        mobile_number = request.form['mobile_number']
+        subscriptions(mobile_number,first_name,last_name)
+        return render_template('index.html')
+
+    return render_template('index.html')
+
+def subscriptions(mobile_number,first_name,last_name):
+    trm_obj = Trumpia()
+    body = {
+        'list_name':'ContactsList',
+        'subscriptions':[
+            {
+                "first_name": first_name,
+                "last_name": last_name,
+                "mobile":
+                {
+                    "number":mobile_number,
+                    "country_code":"1"
+                },
+                "voice_device": "mobile"
+            }
+        ]
+    }
+    subscription_status = trm_obj.getSearchSubscription(mobile_number)
+    if 'MPSE2305' in subscription_status :
+        print("PUT SUB")
+        request_id = trm_obj.putSubscription(body)
+        time.sleep(2)
+        subscription_id = trm_obj.getStatusReport(request_id)
+    elif 'subscription_id_list' in subscription_status:
+        print("POST SUB: ")
+        subscription_id = subscription_status['subscription_id_list']
+        subscription_id = str(subscription_id).strip("['']")
+        subscription_data = trm_obj.getSubscription(subscription_id)
+        current_mobile_number = subscription_data['mobile']['value']
+        if current_mobile_number != mobile_number:
+            body = {
+                'list_name':'ContactsList',
+                'subscriptions':[
+                    {
+                        "mobile":
+                        {
+                            "number":mobile_number,
+                            "country_code":"1"
+                        },
+                        "voice_device": "mobile"
+                    }
+                ]
+            }
+            request_id = trm_obj.postSubscription(subscription_id,body)
+            subscription_id = trm_obj.getStatusReport(request_id)
+
+        current_list_ids = subscription_data['list_ids']
+        if 'first_name' in subscription_data:
+            current_first_name = subscription_data['first_name']
+            if current_first_name == first_name:
+                pass
+            else:
+                body = {
+                    'list_name':'ContactsList',
+                    'subscriptions':[
+                        {
+                            "first_name": first_name,
+                        }
+                    ]
+                }
+                request_id = trm_obj.postSubscription(subscription_id,body)
+                subscription_id = trm_obj.getStatusReport(request_id)
+        if 'last_name' in subscription_data:
+            current_last_name = subscription_data['last_name']
+            if current_first_name == first_name:
+                pass
+            else:
+                body = {
+                    'list_name':'ContactsList',
+                    'subscriptions':[
+                        {
+                            "last_name": last_name,
+                        }
+                    ]
+                }
+                request_id = trm_obj.postSubscription(subscription_id,body)
+                subscription_id = trm_obj.getStatusReport(request_id)
+        if 'last_name' not in subscription_data:
+            body = {
+                'list_name':'ContactsList',
+                'subscriptions':[
+                    {
+                        "last_name": last_name,
+                    }
+                ]
+            }
+            request_id = trm_obj.postSubscription(subscription_id,body)
+            subscription_id = trm_obj.getStatusReport(request_id)
+    else:
+        print(subscription_status)
+
+
+if __name__ == '__main_':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
---
+++ trumpia_Utility/__init__.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+import requests, json, time,os
+
+PATH = os.getcwd()
+USERNAME = ''
+TRMURL = 'https://api.trumpia.com/rest/v1/'+USERNAME+'/'
+APIKEY = ''
+HEADER = {
+    'Content-Type': 'application/json',
+    'x-apikey': APIKEY
+}
+METHOD = ['PUT','POST','GET','DELETE']
+FUNCTION = ['subscription','report']
+
+class Trumpia:
+
+    def __init__(self):
+        pass
+
+    def getSearchSubscription(self,mobile_number):
+        retry = 0
+        if len(mobile_number) <= 0:
+            print(mobile_number)
+            return
+        while retry < 3:
+            try:
+                response = requests.request(METHOD[2],TRMURL + FUNCTION[0]+'/'+'search?search_type=2&search_data='+mobile_number,headers = HEADER)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print('Error: {}'.format(str(err)))
+            except requests.exceptions.Timeout as err_time:
+                print('Error: {}'.format(str(err_time)))
+
+            if response.status_code == 200:
+                json_response = response.json()
+                if 'status_code' in json_response:
+                    status_code = json_response['status_code']
+                    print('trumpia_Utility | Status code: {}'.format(status_code))
+                    subscription_status = self.subscriptionStatusCodes(status_code)
+                    print('trumpia_Utility | {}'.format(status_code + ': ' + subscription_status))
+                    resp = str(status_code) + ': ' +str(subscription_status)
+                    return resp
+                elif 'subscription_id_list' in json_response:
+                    print('trumpia_Utility | {}'.format(json_response))
+                    return json_response
+                else:
+                    pass
+
+            else:
+                retry+=1
+                print('Retry: {}'.format(retry))
+
+
+    def getSubscription(self,subscription_id):
+        retry = 0
+        if len(subscription_id) <= 0:
+            print(subscription_id)
+            return
+        while retry < 3:
+            try:
+                response = requests.request(METHOD[2],TRMURL + FUNCTION[0]+'/'+subscription_id,headers = HEADER)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print('trumpia_Utility | Error: {}'.format(str(err)))
+            except requests.exceptions.Timeout as err_time:
+                print('trumpia_Utility | Error: {}'.format(str(err_time)))
+
+            if response.status_code == 200:
+                json_response = response.json()
+                print(json_response)
+                print('trumpia_Utility GET SUB BY ID| json_response: {}'.format(json_response))
+                return json_response
+            else:
+                retry+=1
+                print('trumpia_Utility | Retry: {}'.format(retry))
+
+
+    def putSubscription(self,body):
+        retry = 0
+        if len(body) <= 0:
+            print(body)
+            return
+        while retry < 3:
+            try:
+                response = requests.request(METHOD[0],TRMURL + FUNCTION[0],json = body,headers = HEADER)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print('trumpia_Utility | Error: {}'.format(str(err)))
+            except requests.exceptions.Timeout as err_time:
+                print('trumpia_Utility | Error: {}'.format(str(err_time)))
+
+            if response.status_code == 200:
+                json_response = response.json()
+                print(json_response)
+                request_id = json_response['request_id']
+                print('trumpia_Utility | Request id: {}'.format(request_id))
+                return request_id
+            else:
+                retry+=1
+                print('trumpia_Utility | Retry: {}'.format(retry))
+
+    def postSubscription(self,subscription_id,body):
+        retry = 0
+        if len(str(subscription_id)) <= 0 or len(body) <= 0:
+            print(subscription_id)
+            return
+        while retry < 3:
+            try:
+                response = requests.request(METHOD[1],TRMURL + FUNCTION[0]+'/'+str(subscription_id),json = body,headers = HEADER)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print('trumpia_Utility | Error: {}'.format(str(err)))
+            except requests.exceptions.Timeout as err_time:
+                print('trumpia_Utility | Error: {}'.format(str(err_time)))
+
+            if response.status_code == 200:
+                json_response = response.json()
+                print(json_response)
+                request_id = json_response['request_id']
+                print('trumpia_Utility | Request id: {}'.format(request_id))
+                return request_id
+            else:
+                retry+=1
+                print('trumpia_Utility | Retry: {}'.format(retry))
+
+    def getStatusReport(self,request_id):
+        retry = 0
+        if len(request_id) <= 0:
+            print(request_id)
+            return
+        while retry < 3:
+            try:
+                response = requests.request(METHOD[2],TRMURL + FUNCTION[1]+'/'+request_id,headers = HEADER)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print('trumpia_Utility | Error: {}'.format(str(err)))
+            except requests.exceptions.Timeout as err_time:
+                print('trumpia_Utility | Error: {}'.format(str(err_time)))
+
+            if response.status_code == 200:
+                json_response = response.json()
+                if 'status_code' in json_response:
+                    status_code = json_response['status_code']
+                    print('trumpia_Utility | Status code: {}'.format(status_code))
+                    subscription_status = self.subscriptionStatusCodes(status_code)
+                    print(status_code + ': ' + subscription_status)
+                    return
+                if 'subscription_id' in json_response:
+                    subscription_id = json_response['subscription_id']
+                    print('trumpia_Utility |POST SUBSCRIPTION ID:  {}'.format(subscription_id))
+                    return subscription_id
+
+                for data in json_response:
+                    #FAILED PUT SUBSCRIPTION
+                    if 'status_code' in data:
+                        status_code = data['status_code']
+                        print('trumpia_Utility |Status code: {}'.format(status_code))
+                        subscription_status = self.subscriptionStatusCodes(status_code)
+                        print(status_code + ': ' + subscription_status)
+                    #SUCESSFUL PUT SUBSCRIPTION
+                    if 'subscription_id' in data:
+                        subscription_id = data['subscription_id']
+                        print('SUCCESS PUT SUBSCRIPTION ID:  {}'.format(subscription_id))
+                        return subscription_id
+                break
+            else:
+                retry+=1
+                print('Retry: {}'.format(retry))
+
+    def subscriptionStatusCodes(self,status_code):
+        file = open(PATH+'/trumpia_Utility/subscriptionStatusCodes.txt','r')
+        subscription_status_codes = {}
+        for codes in file:
+            key, value = codes.split('\t')
+            subscription_status_codes[key] = value
+
+        if status_code in subscription_status_codes:
+            subscription_status = subscription_status_codes[status_code]
+        else:
+            print('Status code: {} not in subscriptionStatusCodes'.format(status_code))
+
+        return subscription_status
